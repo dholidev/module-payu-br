2ba65848bd599d984025f68a8d8f24c27daf67e9
dholi <comunidade@dholi.dev>
2021-02-24 15:32:05 -0300

    Parcelamento para PayU Latam

--- src/module-payu-br/Gateway/Response/Payment/BoletoAuthorisationDetailsHandler.php
+++ src/module-payu-br/Gateway/Response/Payment/BoletoAuthorisationDetailsHandler.php
@@ -5,8 +5,8 @@
 * 
 * @category     Dholi
 * @package      Modulo PayUBr
-* @copyright    Copyright (c) 2020 dholi (https://www.dholi.dev)
-* @version      1.0.0
+* @copyright    Copyright (c) 2021 dholi (https://www.dholi.dev)
+* @version      1.1.0
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
